38ce1ffb73726c0f465fb7641adde645253f11df
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-16 09:02:39 -0400

    Update _Citrix_XA-XD_Commands_ Examples.ps1

--- _Citrix_XA-XD_Commands_ Examples.ps1
+++ _Citrix_XA-XD_Commands_ Examples.ps1
@@ -2,7 +2,7 @@
   Get-BrokerSession -AdminAddress BrokerController01 | Select-Object -Property AppState,SessionState,SessionType, @{Name='ApplicationName';E={$_.ApplicationsInUse}}, MachineName, UserFullName, UserName, UntrustedUserName 
 
 # Example: Get specified users session
-  Get-BrokerSession -AdminAddress BrokerController01 | Where-Object {$_.UserName -eq "DomainName\userID"} 
+  Get-BrokerSession -AdminAddress BrokerController01 | Where-Object {$_.UserName -eq "DomainName\userID"} | Select-Object -Property AppState,SessionState,SessionType, @{Name='ApplicationName';E={$_.ApplicationsInUse}}, MachineName, UserFullName, UserName, UntrustedUserName  
 
 # Example: Stops or Logs Off specified users session
   Get-BrokerSession -AdminAddress BrokerController01 | Where-Object {$_.UserName -eq "DomainName\userID"} | Stop-BrokerSession 
