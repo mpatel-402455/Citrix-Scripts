76920ae09df24379f4022b822391c125ba40951e
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-11-14 14:15:10 -0500

    Update _Citrix_XA-XD_Commands_ Examples.ps1

--- _Citrix_XA-XD_Commands_ Examples.ps1
+++ _Citrix_XA-XD_Commands_ Examples.ps1
@@ -15,3 +15,8 @@
 
 #Example: Shows Published Applications report including, WorkerGroup, Servers, DisplayName, Discription, and Users
 Get-XAApplicationReport -ComputerName CtxXML01.lab.com -BrowserName "MS Word 2016" | select-object WorkerGroupNames, @{n=”Servers”;e={[string]::join(” ; “, $_.ServerNames)}}, DisplayName, Description, @{n=”Users”;e={[string]::join(” ; “, $_.Accounts)}}
+
+
+# Example: Create CSV inventory for Citrix v6.5 XenApp info
+  Get-XAWorkerGroup | select workergroupname, @{n = 'server' ; E = {$_.servernames -join ','}}, @{n= 'OU' ; E = {$_.OUs -join ','}} | Export-Csv c:\temp\wg-list-MVP2.csv
+  Get-XAApplicationReport * | select ApplicationType, DisplayName, FolderPath, Enabled, HideWhenDisabled, ContentAddress, CommandLineExecutable, WorkingDirectory, AnonymousConnectionsAllowed, AddToClientStartMenu, ClientFolder, StartMenuFolder, AddToClientDesktop, ConnectionsThroughAccessGatewayAllowed, OtherConnectionsAllowed, AccessSessionConditionsEnabled, @{n="AccessSessionConditions";e={[string]::join(" ; ", $_.AccessSessionConditions)}}, InstanceLimit, MultipleInstancesPerUserAllowed, CpuPriorityLevel, AudioType, AudioRequired, SslConnectionEnabled, EncryptionLevel, EncryptionRequired, WaitOnPrinterCreation, WindowType, ColorDepth, TitleBarHidden, MaximizedOnStartup, OfflineAccessAllowed, CachingOption, AlternateProfiles, RunAsLeastPrivilegedUser, @{n="Servers";e={[string]::join(" ; ", $_.ServerNames)}}, @{n="WorkerGroups";e={[string]::join(" ; ", $_.WorkerGroupNames)}}, @{n="Users";e={[string]::join(" ; ", $_.Accounts)}} | Export-Csv c:\temp\app-list-MVP2.csv
